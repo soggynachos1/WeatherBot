34dbc4dbebfddea361a4008c7fc8f7b8f812825a
Jayson C <144086481+MONKEYFACE678@users.noreply.github.com>
2025-10-15 17:08:16 -0600

    Added comments and fixed file access
    Added comments as I forgot to initially, then fixed the file access due to activities being moved to a folder

--- Weather.py
+++ Weather.py
@@ -1,14 +1,19 @@
 import requests
 import random
 
+#needed to access api
 headers = {'User-Agent' : 'jaysonc678@gmail.com'}
+#link to data
 endpoint = 'https://api.weather.gov/gridpoints/EPZ/124,104/forecast'
 
+#get info from api then get specifically the first period
 response = requests.get(endpoint, headers = headers)
 data = response.json()["properties"]["periods"][0]
 
+#save the forecast in lowercase
 weather = data["shortForecast"].lower()
 
+#raise error if api failed
 if response:
     print("API accessed successfully.")
 else:
@@ -16,6 +21,8 @@ else:
 
 print(f"It is {weather} outside")
 
-with open(weather.lower() + ".txt", 'r') as f:
+#open activities file based on forecast, choose random activity from there and print it
+with open("WeatherActivities/" + weather.lower() + ".txt", 'r') as f:
     activities = f.read().split('\n')
+
     print(f"An activity you can do in this weather is {random.choice(activities)}.")
