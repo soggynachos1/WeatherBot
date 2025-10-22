3bf7af2abbf775d17ae695bcaa41c2c6cc375fa5
T <JAYSONC678@GMAIL.COM>
2025-10-22 17:05:04 -0600

    Get weather can take coords

--- Weather.py
+++ Weather.py
@@ -1,6 +1,7 @@
 import requests
 import random
 
+
 #api setup
 #---------------------------------------------------------------
 def get_data_as_json(endpoint, headers):
@@ -17,23 +18,24 @@ user_agent = "jaysonc678@gmail.com"
 BASE_URL = "https://api.weather.gov"
 headers = {'User-Agent' : f'{user_agent}'}
 
-#get coordinates to gridpoints
-#--------------------------------------------------------------------------------
-coordinates = [32.9004,-105.9629]
-coordinate_endpoint = f"{BASE_URL}/points/{coordinates[0]},{coordinates[1]}"
+def coords_to_gridpoints(coordinates):
+    coordinate_endpoint = f"{BASE_URL}/points/{coordinates[0]},{coordinates[1]}"
 
-coordinate_data = get_data_as_json(coordinate_endpoint, headers)["properties"]
-station_and_coords = [coordinate_data["gridId"], coordinate_data["gridX"], coordinate_data["gridY"]]
+    coordinate_data = get_data_as_json(coordinate_endpoint, headers)["properties"]
+    station_and_coords = [coordinate_data["gridId"], coordinate_data["gridX"], coordinate_data["gridY"]]
+    return station_and_coords
 
 #get forecast from gridpoints
+def forecast_from_gridpoints(station_and_coords):
 #-------------------------------------------------------------------------------------------
-forecast_endpoint = f"{BASE_URL}/gridpoints/{station_and_coords[0]}/{station_and_coords[1]},{station_and_coords[2]}/forecast"
-#get info from api then get specifically the first period
-forecast_data = get_data_as_json(forecast_endpoint, headers)["properties"]["periods"][0]
+    forecast_endpoint = f"{BASE_URL}/gridpoints/{station_and_coords[0]}/{station_and_coords[1]},{station_and_coords[2]}/forecast"
+    #get info from api then get specifically the first period
+    forecast_data = get_data_as_json(forecast_endpoint, headers)["properties"]["periods"][0]
 
-#save the forecast in lowercase
-weather = forecast_data["shortForecast"].lower()
-
-def get_weather():
+    #save the forecast in lowercase
+    weather = forecast_data["shortForecast"].lower()
     return weather
+
+def get_weather(coords = [32.9004,-105.9629]):
+    return forecast_from_gridpoints(coords_to_gridpoints(coords))
 
